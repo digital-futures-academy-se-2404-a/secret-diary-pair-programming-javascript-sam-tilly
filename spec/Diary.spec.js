748b99d824b3b464f8ff7b051e5515d6c02535cf
Sam K <samkoppenhout@live.com>
2024-04-23 15:26:37 +0100

    Passing failing test to Tilly

---
+++ spec/Diary.spec.js
@@ -0,0 +1,12 @@
+describe("Diary Entry Tests", () => {
+    it("should be able to increase the number of entries when addEntry is called", () => {
+        // Arrange
+        const testDiary = new Diary();
+        const testEntry = "Dear diary";
+        const expected = testDiary.getEntries().length + 1;
+        // Act
+        testDiary.addEntry(testEntry);
+        // Assert
+        expect(testDiary.getEntries().length).toBe(expected);
+    });
+});

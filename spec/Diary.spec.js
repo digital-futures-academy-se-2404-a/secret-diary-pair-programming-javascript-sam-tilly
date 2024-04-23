2be131d28583a3f9e89bf984fb6430c474559650
Sam K <samkoppenhout@live.com>
2024-04-23 16:58:38 +0100

    Passing test to Tilly

--- spec/Diary.spec.js
+++ spec/Diary.spec.js
@@ -12,35 +12,51 @@ describe("Diary Entry Tests", () => {
         testEntry = undefined;
         expected = undefined;
     });
+    describe("Add Entry Tests", () => {
+        it("should be able to increase the number of entries when addEntry is called", () => {
+            // Arrange
+            testEntry = "Dear diary";
+            expected = testDiary.getEntries().length + 1;
+            // Act
+            testDiary.addEntry(testEntry);
+            // Assert
+            expect(testDiary.getEntries().length).toBe(expected);
+        });
+        it("should add the same entry to the entries as is entered", () => {
+            // Arrange
+            testEntry = "Dear diary";
+            expected = testEntry;
+            // Act
+            testDiary.addEntry(testEntry);
+            // Assert
+            expect(testDiary.getEntries()[0]).toBe(testEntry);
+        })
 
-  it("should be able to increase the number of entries when addEntry is called", () => {
-      // Arrange
-        testEntry = "Dear diary";
-        expected = testDiary.getEntries().length + 1;
-        // Act
-        testDiary.addEntry(testEntry);
-        // Assert
-        expect(testDiary.getEntries().length).toBe(expected);
-  });
-    
-    it("should add the same entry to the entries as is entered", () => {
-        // Arrange
-        testEntry = "Dear diary";
-        expected = testEntry;
-        // Act
-        testDiary.addEntry(testEntry);
-        // Assert
-        expect(testDiary.getEntries()[0]).toBe(testEntry);
-    })
-
-    it("should not allow a null entry to be added", () => {
-        // Arrange
-        testEntry = null;
-        expected = 0;
-        // Act
-        testDiary.addEntry(testEntry);
-        // Assert
-        expect(testDiary.getEntries().length).toBe(expected);
+        it("should not allow a null entry to be added", () => {
+            // Arrange
+            testEntry = null;
+            expected = 0;
+            // Act
+            testDiary.addEntry(testEntry);
+            // Assert
+            expect(testDiary.getEntries().length).toBe(expected);
+        });
+    });
+    describe("Add Entry Tests", () => {
+        it("should be able to fetch all of the diary entries", () => {
+            // Arrange
+            testDiary.addEntry("Dear diary 1");
+            testDiary.addEntry("Dear diary 2");
+            testDiary.addEntry("Dear diary 3");
+            expected = 3;
+            // Act
+            // Assert
+            expect(testDiary.getEntries().length).toBe(expected);
+        });
+        it("should be able to print out all of the diary entries", () => {
+            
+        })
     });
 });
+    
 

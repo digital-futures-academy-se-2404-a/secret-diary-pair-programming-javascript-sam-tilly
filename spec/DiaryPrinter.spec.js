bdd3e6d1d89e3fe708c041fad6e334ae16985f66
Tilly Mallabar <tilly.m94@gmail.com>
2024-04-24 11:23:02 +0100

    FAILED Entry test 1

--- spec/DiaryPrinter.spec.js
+++ spec/DiaryPrinter.spec.js
@@ -1,5 +1,6 @@
 describe("DiaryPrinter Class Tests", () => {
     it("the counter should show the number of entries that are to be printed", () => {
-        
+        // static properties of diary printer class 
+        //
     })
 });
---
+++ spec/Entry.spec.js
@@ -0,0 +1,12 @@
+describe("Diary Entry Tests", () => {
+    it("getContents should return entry contents", () => {
+        // Arrange
+        let testEntry = "Dear Diary";
+        let entry = new Entry(testEntry);
+        // Act
+        
+        // Assert
+        expect(entry.getContents()).toBe(testEntry);
+    });
+
+});
